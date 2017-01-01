8adbcf8e7439428fd8c9dea60b4f9a2e0a0d7bae
Scott González <scott.gonzalez@gmail.com>
2017-01-01 11:12:59 -0500

    Rename `__constant__` animation to `constant`

--- src/animation/constant.js
+++ src/animation/constant.js
@@ -2,7 +2,7 @@
 // This is implemented as an animation so that `LightDisplay` doesn't need
 // special logic for switching between an animation and constant on.
 module.exports = ({ five, Animation }) => {
-  Animation.define('__constant__', class Solid extends Animation {
+  Animation.define('constant', class Solid extends Animation {
     start () {
       this.segments.forEach((segment) => segment.on())
     }
